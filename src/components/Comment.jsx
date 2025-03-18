739a44ded47e86d1d3340a526bfc8419b1a2d3e0
Felipe <felipermpires@gmail.com>
2025-03-18 09:55:37 -0300

    correção no json e funcão delete comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,11 @@ import styles from "./Comment.module.css"
 import { ThumbsUp, Trash } from "phosphor-react"
 import { Avatar } from "./Avatar"
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
@@ -18,7 +22,8 @@ export function Comment({ content }) {
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              {" "}
               <Trash size={24} />
             </button>
           </header>
